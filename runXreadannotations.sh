abb7899b44596ece03ed233cec79ebbba7e0b350
Danelle Cline <dcline@mbari.org>
2018-07-16 18:16:25 -0700

    changed to mbari/avedac-mbarivision-readannotations

--- runXreadannotations.sh
+++ runXreadannotations.sh
@@ -2,5 +2,5 @@
 # Run docker image with X variant in detached mode
 #!/bin/bash
 # Run docker image with X variant in detached mode
-CID=$(docker run -d -v $PWD/data:/data -v $PWD/scripts:/scripts -p 22 readannotations)
+CID=$(docker run -d -v $PWD/data:/data -v $PWD/scripts:/scripts -p 22 mbari/avedac-mbarivision-readannotations)
 echo $CID
