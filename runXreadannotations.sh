61695d629deac59343a630ebced903e75ff6c6c9
resteybar <resteybar@csumb.edu>
2018-07-17 19:41:37 -0700

    New Update

--- runXreadannotations.sh
+++ runXreadannotations.sh
@@ -2,5 +2,5 @@
 # Run docker image with X variant in detached mode
 #!/bin/bash
 # Run docker image with X variant in detached mode
-CID=$(docker run -d -v $PWD/data:/data -v $PWD/scripts:/scripts -p 22 mbari/avedac-mbarivision-readannotations)
+CID=$(docker run -d -v /Users/Gray/Documents/MBARI/avedac-mbarivision/data:/data -v /Users/Gray/Documents/MBARI/avedac-mbarivision/scripts:/scripts -p 22 mbari/avedac-mbarivision-readannotations)
 echo $CID
--- scripts/process_midwater_readannotations.sh
+++ scripts/process_midwater_readannotations.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-readAnnotations --input-frames=.....  your data is in /data this script is in /scripts
+code/src/readAnnotations.C --in=raster:/tmp/data/f#.png --out=raster:/tmp/data/all_results/ --input-frames=1-898@1  --output-frames=1-898@1 --mbari-save-results --mbari-save-output --mbari-save-events-xml=results.xml
