38cbdf84a324bb975f8de8a0e53b49791301ec6d
resteybar <resteybar@csumb.edu>
2018-07-18 14:39:03 -0700

    Added changes for running into xQuartz

--- scripts/process_midwater_readannotations.sh
+++ scripts/process_midwater_readannotations.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-readAnnotations --input-frames=.....  your data is in /data this script is in /scripts
+export DISPLAY=docker.for.mac.localhost:0
+export AVED_BIN=/usr/local/bin
+readAnnotations --in=raster:/data/f#.png \
+--out=raster:/data/all_results/ \
+--input-frames=1-898@1 \
+--output-frames=1-898@1 \
+--mbari-save-results \
+--mbari-save-output \
+--mbari-save-events-xml=/data/all_results/results.xml \
+--mbari-display-results --logverb=Debug \
+--mbari-rescale-display=320x240 \
+--mbari-tracking-mode=NearestNeighborHough \
+--mbari-segment-algorithm-input-image=Luminance \
+--mbari-min-event-frames=3 --mbari-color-space=RGB \
+--mbari-se-size=4 \
+--mbari-cache-size=120 \
+--mbari-segment-algorithm=Best --mbari-rescale-display=320x240 \
+--mbari-min-event-frames=1 \
+--mbari-segment-graph-parameters=0.75,500,250 \
+--mbari-segment-adaptive-parameters=2,2 --mbari-dynamic-mask=true
