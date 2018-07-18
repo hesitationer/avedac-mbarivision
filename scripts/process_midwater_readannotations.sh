abc785d45ee6767f5dfcb6e1677d4b3670fdad3e
Raymond Esteybar <resteybar@csumb.edu>
2018-07-17 19:32:07 -0700

    *Fixed an error regarding AVED_BIN

--- scripts/process_midwater_readannotations.sh
+++ scripts/process_midwater_readannotations.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-readAnnotations --input-frames=.....  your data is in /data this script is in /scripts
+export AVED_BIN=/usr/local/bin
+readAnnotations --in=raster:/data/f#.png --out=raster:/data/all_results/ --input-frames=1-898@1 --output-frames=1-898@1 --mbari-save-results --mbari-save-output --mbari-save-events-xml=results.xml
