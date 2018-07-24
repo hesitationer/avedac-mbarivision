d6818d4ef3ac75a0f5e17d7f0165f2df64745cf3
Mayra Ochoa <mochoa@mbari1935.local>
2018-07-24 14:50:59 -0700

    Updated script

--- runXreadannotations.sh
+++ runXreadannotations.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Run docker image with X variant in detached mode
-UN=Gray
+UN=mochoa
 PWD=/Users/$UN/Documents/MBARI/avedac-mbarivision
-CID=$(docker run -d -v $PWD/data:/data -v $PWD/scripts:/scripts -p 22 mbari/avedac-mbarivision-readannotations)
+CID=$(docker run -d -v $PWD/i2data:/i2data -v $PWD/scripts:/scripts -p 22 mbari/avedac-mbarivision-readannotations)
