ce5dfae6b7eaf6a56d202cfa4d61c7e1e5b43794
Danelle Cline <dcline@mbari.org>
2018-09-04 23:02:16 -0700

    removed overloaded PWD

--- runXreadannotations.sh
+++ runXreadannotations.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # Run docker image with X variant in detached mode
-UN=Gray
-PWD=/Users/$UN/Documents/MBARI/avedac-mbarivision
 CID=$(docker run -d -v $PWD/data:/data -v $PWD/scripts:/scripts -p 22 mbari/avedac-mbarivision-readannotations)
